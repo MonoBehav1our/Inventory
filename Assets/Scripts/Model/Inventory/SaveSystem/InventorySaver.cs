0d028758020d8886cf1a0bf9f9467638f9e81f98
Mono_Behaviour雨 <stassuleymanov00@gmail.com>
2024-07-08 15:30:25 +0300

    some fix

--- Assets/Scripts/Model/Inventory/SaveSystem/InventorySaver.cs
+++ Assets/Scripts/Model/Inventory/SaveSystem/InventorySaver.cs
@@ -11,26 +11,13 @@ namespace Model.Inventory.Saves
         {
             string json = JsonUtility.ToJson(data);
             StreamWriter sw = new StreamWriter(PATH, false);
-            Debug.Log(PATH);
-            Debug.Log(json);
             sw.Write(json);
             sw.Close();
-            //PlayerPrefs.SetString(PATH, json);
+            sw.Dispose();
         }
 
         public static InventoryGridData Load()
         {
-            //if (PlayerPrefs.HasKey(PATH))
-            //{
-            //    string save = PlayerPrefs.GetString(PATH);
-            //    InventoryGridData data = JsonUtility.FromJson<InventoryGridData>(save);
-            //    return data;
-            //}
-            //else
-            //{
-
-            //}
-
             if (File.Exists(PATH))
             {
                 StreamReader streamReader = new StreamReader(PATH);
